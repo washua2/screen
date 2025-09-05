22b10ee1679bd5db63dca6dbe0ede29cbb618f0f
washua2 <80311147+washua2@users.noreply.github.com>
2025-09-05 15:18:05 +0800

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -3,8 +3,8 @@ export const MAX_THUMBNAIL_SIZE = 10 * 1024 * 1024;
 
 export const BUNNY = {
   STREAM_BASE_URL: "https://video.bunnycdn.com/library",
-  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/snapcast",
-  CDN_URL: "https://snapcast.b-cdn.net",
+  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/jsm-snapcast-1",
+  CDN_URL: "https://jsm-snapcast-1.b-cdn.net",
   EMBED_URL: "https://iframe.mediadelivery.net/embed",
   TRANSCRIPT_URL: "https://vz-47a08e64-84d.b-cdn.net",
 };
